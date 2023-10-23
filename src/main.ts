6c684777c6ef4e087f6e7009a2e873213b5a303f
Abel Goy <agoy@ucsc.edu>
2023-10-23 16:41:46 -0700

    Refactoring

--- src/main.ts
+++ src/main.ts
@@ -18,90 +18,78 @@ canvas.height = 150;
 const canvasX = 0;
 const canvasY = 0;
 
-const ctx = canvas.getContext("2d");
-ctx!.fillStyle = "yellow";
-ctx?.fillRect(canvasX, canvasY, canvas.width, canvas.height);
+const ctx = canvas.getContext("2d")!;
+ctx.fillStyle = "yellow";
+ctx.fillRect(canvasX, canvasY, canvas.width, canvas.height);
 app.append(canvas);
 
 // Drawing creation
 let isDrawing = false;
 let x = 0;
 let y = 0;
-const drawArray1: number[][] = [];
-const drawArray2: number[][] = [];
-
-const event = new CustomEvent("drawing-changed");
-/*canvas.addEventListener(
-    "drawing-changed",
-    (e) => {
-        // clear and redraw lines
-
-    },
-    false,
-  );*/
-  
-// Dispatch the event.
-canvas.dispatchEvent(event);
-
-
-/*function showLinePoints(x: number[], y: number[], z: number) {
-    console.log(x[z]);
-    console.log(y[z]);
-}*/
-
-  canvas.addEventListener("mousedown", (e) => {
-      x = e.offsetX;
-      y = e.offsetY;
-      isDrawing = true;
-      // Store point #1 (x, y)
-      console.log(x + "," + y);
-      drawArray1.push([x, y]);
-      console.log(drawArray1.pop());
- });
-
- canvas.addEventListener("mousemove", (e) => {
-    if (isDrawing) {
-      drawLine(ctx, x, y, e.offsetX, e.offsetY);
-      x = e.offsetX;
-      y = e.offsetY;
-      // canvas.dispatchEvent(drawingChanged);
+interface Point {
+  x: number;
+  y: number;
+}
+let currentLine: Point[] = [];
+let arrayOfLines: Point[][] = [];
+function notify(name: "drawing-changed") {
+  canvas.dispatchEvent(new Event(name));
+}
+canvas.addEventListener(
+  "drawing-changed",
+  () => {
+    // clear
+    ctx.clearRect(x, y, canvas.width, canvas.height);
+    ctx.fillRect(canvasX, canvasY, canvas.width, canvas.height);
+    // redraw lines
+    for (const line of arrayOfLines) {
+      drawLine(ctx, line);
     }
-  });
-  
-  canvas.addEventListener("mouseup", (e) => {
-    if (isDrawing) {
-        drawLine(ctx, x, y, e.offsetX, e.offsetY);
-        x = e.offsetX;
-        y = e.offsetY;
-        isDrawing = false;
-        // Store point #2 (x, y)
-        console.log(x + "," + y);
-        drawArray2.push([x, y]);
-        console.log(drawArray2.pop());
-    }
-  });
-/*    
-  canvas.addEventListener("drawing-changed", (e) => {
-      // observer will clear
-      ctx?.clearRect(x, y, canvas.width, canvas.height);
-      // and redraw the user's lines
-      for (let i = 0; i < drawArray1.length; i++) {
-          console.log(drawArray1[i]);
-          console.log(drawArray2[i]);
-        //drawLine(ctx, drawArray1[i], drawArray2[i])
-      }
-  });*/
+  },
+  false
+);
 
-  // ESlint doesn't like my use of any, but i disabled the warnings
-  function drawLine(context: any, x1: number, y1: number, x2: number, y2: number) {
-    context.beginPath();
-    //context.strokeStyle = "black";
-    //context.lineWidth = 1;
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
-    //context.closePath();
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+  // initialize array of coordinates
+  currentLine = [];
+  currentLine.push({ x, y });
+  notify("drawing-changed");
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    x = e.offsetX;
+    y = e.offsetY;
+    currentLine.push({ x, y });
+    notify("drawing-changed");
   }
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = false;
+    arrayOfLines.push(currentLine);
+    notify("drawing-changed");
+  }
+});
+
+// ESlint doesn't like my use of any, but i disabled the warnings
+function drawLine(ctx: CanvasRenderingContext2D, points: Point[]) {
+  const [firstPoint, ...remainingPoints] = points;
+  ctx.beginPath();
+  const { x, y } = firstPoint;
+  ctx.moveTo(x, y);
+  for (const { x, y } of remainingPoints) {
+    ctx.lineTo(x, y);
+  }
+  ctx.stroke();
+}
 
 //onmousemove = (event) => { };
 
@@ -111,10 +99,9 @@ const mainButton = document.createElement("button");
 mainButton.innerHTML = button;
 app.append(mainButton);
 mainButton.addEventListener("click", () => {
-    ctx?.clearRect(x, y, canvas.width, canvas.height);
-    ctx?.fillRect(canvasX, canvasY, canvas.width, canvas.height);
+  arrayOfLines = [];
+  notify("drawing-changed");
 });
-
 
 // FUTURE STEPS:
 // Store the 2 points
@@ -123,3 +110,4 @@ mainButton.addEventListener("click", () => {
 // draw everything
 // canvas addeventlistener redraw
 // be cool
+// (0,0) -> (0, 1) -> (0,2)
